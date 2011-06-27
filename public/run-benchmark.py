215ddbe889072bbfbd86af80f765fc4a6afea5a6
Terry Ma <zhenchuan.ma@gmail.com>
2011-06-27 16:53:26 -0700

    add run benchmark script and more test results

--- public/run-benchmark.py
+++ public/run-benchmark.py
@@ -1,4 +1,29 @@
 from subprocess import *
+import time
 
-instance_types = ['m1.small', 'm1.large', 'm1.xlarge']
-file_systems = ['ext3', 'ext4']
+# This script will run the benchmarking on every combination that's supported, and writes the output to ../log
+
+# instance_types = ['m1.small', 'm1.large', 'm1.xlarge']
+# file_systems = ['ext3', 'ext4', 'xfs']
+instance_types = ['m1.small']
+file_systems = ['ext3']
+tenacies = ['dedicated', 'default']
+
+processes = []
+
+for i in instance_types:
+    for f in file_systems:
+        for t in tenacies:
+            # command = "python create-instance.py -i %s -f %s -t %s > ../logs/%s-%s-%s.log" % (i, f, t, i, f, t)
+            command = "python create-instance.py -i %s -f %s -t %s" % (i, f, t)
+            print "Executing %s" % command
+            p = Popen(command, shell=True)
+            processes.append(p)
+            print "Sleeping for 5 seconds..."
+            time.sleep(5)
+
+for p in processes:
+    print "Waiting for process %s to finish..." % p.pid
+    p.wait()
+
+
---
+++ public/run-benchmark.sh
@@ -0,0 +1,15 @@
+instance_types=( m1.small m1.large m1.xlarge c1.xlarge )
+file_systems=( ext3 ext4 xfs )
+tenancies=( dedicated default )
+
+for i in "${instance_types[@]}"
+do
+    for f in "${file_systems[@]}"
+    do
+        for t in "${tenancies[@]}"
+        do
+            python create-instance.py -i $i -f $f -t $t > ../logs/$i-$f-$t.out 
+        done
+    done
+done
+
